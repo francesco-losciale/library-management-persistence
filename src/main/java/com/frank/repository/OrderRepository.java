2f0939985f231da425178a2ecb8d9672fb296129
Francesco Losciale <francesco.losciale@gmail.com>
2018-10-02 17:59:15 -0400

    First working unit test

--- src/main/java/com/frank/repository/OrderRepository.java
+++ src/main/java/com/frank/repository/OrderRepository.java
@@ -1,12 +1,18 @@
 package com.frank.repository;
 
-import com.frank.book.Order;
+import com.frank.capabilities.Dehydrator;
+import com.frank.capabilities.Hydratable;
+import com.frank.capabilities.Hydrator;
+import com.frank.context.book.Order;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.types.Decimal128;
 
-public class OrderRepository {
+import java.math.BigDecimal;
+
+public class OrderRepository implements Hydrator, Dehydrator {
 
     private MongoDatabase mongoDatabase;
 
@@ -16,19 +22,41 @@ public class OrderRepository {
         mongoDatabase.createCollection("orders");
     }
 
-    public void save(Order order, Object id) {
+    public void dehydrate(Hydratable hydratable) {
+        Order order = Order.class.cast(hydratable);
+        save(order, id(order));
+    }
+
+    public Hydratable hydrate(Object id) {
+        return read(id);
+    }
+
+    private void save(Order order, Object id) {
         Document document = new Document("price", order.getBooksPrice())
-                .append("state", order.getOrderState())
+                .append("receivedByCourier", order.getOrderState().isReceived())
                 .append("id", id);
         mongoDatabase.getCollection("orders").insertOne(document);
     }
 
-    public Order read(Object id) {
+    private Order read(Object id) {
         final Order order = new Order();
         FindIterable<Document> orderFindIterable = mongoDatabase.getCollection("orders").find(new Document("id", id));
         for (Document document : orderFindIterable) {
-            order.hydrateFrom(document);
+            Boolean isReceivedByCourier = document.getBoolean("receivedByCourier");
+            if (isReceivedByCourier) {
+                order.getOrderState().received();
+            }
+            BigDecimal price = ((Decimal128)document.get("price")).bigDecimalValue();
+            // TODO fix: you are not in the right context to write the price in Order
         }
         return order;
+    }
+
+    private Object id(Order order) {
+        Object id = order.getId();
+        if (id != null) {
+            return id;
+        }
+        return "1"; // TODO generate
     }
 }
--- src/test/groovy/com/frank/integration/test/OrderRepositorySpec.groovy
+++ src/test/groovy/com/frank/integration/test/OrderRepositorySpec.groovy
@@ -1,21 +1,14 @@
 package com.frank.integration.test
 
-import com.frank.book.Book
-import com.frank.book.BookCollection
-import com.frank.book.Order
+import com.frank.context.book.Book
+import com.frank.context.book.BookCollection
+import com.frank.context.book.Order
 import com.frank.repository.OrderRepository
 import spock.lang.Specification
 
 class OrderRepositorySpec extends Specification {
 
     final Book book = createBookMock()
-
-    void setup() {
-        book.setTitle("title")
-        book.setAuthor("author")
-        book.setIsbn("isbn")
-        book.setActualPrice(BigDecimal.valueOf(10.45))
-    }
 
     def "given an Order object and a persistence unit when save operation is triggered then the object is persisted"() {
         given: "A set of book for ordering"
@@ -27,7 +20,7 @@ class OrderRepositorySpec extends Specification {
 
         when: "An order is filled in"
         Order order = new Order()
-        order.add(bookCollection)
+        //order.add(bookCollection)
         String orderId = "orderId"
 
         and: "The order is persisted"
